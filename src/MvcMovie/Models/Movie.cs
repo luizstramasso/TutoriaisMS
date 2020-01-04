d62b3d9b5ce36a572a4fef4bb3cae21810b26242
Luiz Stramasso <luizstramasso@gmail.com>
2020-01-04 19:42:39 -0300

    Tutorial MVC com o EF Core 8

--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -7,17 +7,28 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int MovieID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
         [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleseDate { get; set; }
-        public string Genre { get; set; }
 
+        [Range(1, 100)]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string Genre { get; set; }
+
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string Rating { get; set; }
     }
 }
